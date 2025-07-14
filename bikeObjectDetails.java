4168416cfcf3a17a40eb3cfd6a981d84db715d11
Aakashvscode <aakashsamuthirapandi@gmail.com>
2025-07-14 19:27:39 +0530

    updated all files

--- bikeObjectDetails.java
+++
@@ -1,8 +0,0 @@
-public class bikeObjectDetails
-{
-    public static void main(String[] args) {
-        bikeObject reference = new bikeObject();
-        System.out.println(reference);
-        reference.details();
-    }
-}
